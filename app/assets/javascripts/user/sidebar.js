4f8220183f1e971afb8f9b2f50df81cba7f4e63d
vagrant <vagrant@localhost.localdomain>
2020-11-15 13:29:06 +0000

    (fix)item_sidebar

--- app/assets/javascripts/user/sidebar.js
+++ app/assets/javascripts/user/sidebar.js
@@ -2,9 +2,9 @@ document.addEventListener("turbolinks:load", function() {
 	// ↑リロードでJSが効かなくなるのを防止するコマンド
 $(function(){
   $('.sidebar').hover(function(){
-    $('.sidebar').animate({'marginLeft':'200px'},500);},
+    $('.sidebar').stop().animate({'marginLeft':'200px'},500);},
     function () {
-        $('.sidebar').animate({'marginLeft':'0'},500);}
+        $('.sidebar').stop().animate({'marginLeft':'0'},500);}
       );
 });
 })
