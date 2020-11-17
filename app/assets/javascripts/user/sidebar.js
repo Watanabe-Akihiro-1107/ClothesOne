c971de83631dded7e6f18f318763ba1837d0a99b
vagrant <answer.is.near.2@gmail.com>
2020-11-17 14:12:23 +0900

    (fix)sidebar2

--- app/assets/javascripts/user/sidebar.js
+++ app/assets/javascripts/user/sidebar.js
@@ -2,9 +2,9 @@ document.addEventListener("turbolinks:load", function() {
 	// ↑リロードでJSが効かなくなるのを防止するコマンド
 $(function(){
   $('.sidebar').hover(function(){
-    $('.sidebar').stop().animate({'marginLeft':'200px'},500);},
+    $('.sidebar').stop().animate({'marginLeft':'-30px'},500);},
     function () {
-        $('.sidebar').stop().animate({'marginLeft':'0'},500);}
+        $('.sidebar').stop().animate({'marginLeft':'-250'},500);}
       );
 });
 })
