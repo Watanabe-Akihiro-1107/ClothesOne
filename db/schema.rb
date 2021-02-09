b64dd285672910c91fdad5191e634b117512e9a8
vagrant <answer.is.near.2@gmail.com>
2021-02-10 00:40:29 +0900

    (fix_schema)column_item_price

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_27_131459) do
+ActiveRecord::Schema.define(version: 2021_02_09_152832) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -67,7 +67,7 @@ ActiveRecord::Schema.define(version: 2020_11_27_131459) do
     t.integer "user_id", null: false
     t.integer "brand_id"
     t.integer "category_id", null: false
-    t.string "price", null: false
+    t.string "price"
     t.text "introduction", null: false
     t.string "image_id", null: false
     t.datetime "created_at", null: false
