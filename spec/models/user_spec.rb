18a20bce4f735bbf3fb242be25a94fe2b55c5fdd
vagrant <answer.is.near.2@gmail.com>
2021-02-21 17:33:22 +0900

    (add_rspec)model_user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,11 +11,11 @@ RSpec.describe User, type: :model do
     end
   end
 
-  describe "バリデーションのチェック" do
-  	let(:user) {create(:user)}
+  describe "バリデーションのテスト" do
+  	let(:user) {build(:user)}
   	subject{ test_user.valid? }
 
-  	context "nicknameカラムのチェック" do
+  	context "nicknameカラムのバリデーションのテスト" do
   	let(:test_user) { user }
   	 it "空欄でないこと" do
   	 	test_user.nickname = ""
@@ -31,6 +31,31 @@ RSpec.describe User, type: :model do
   	 	test_user.nickname = Faker::Lorem.characters(number:21)
   	 	is_expected.to eq false
   	 end
+  	end
+
+  	context "passwordカラムのバリデーションのテスト" do
+  	let(:test_user) { user }
+  		it "空欄でないこと" do
+  			test_user.password = ""
+  			is_expected.to eq false
+  		end
+  		it "6文字以上であること" do
+  			test_user.password =Faker::Lorem.characters(number:5)
+  			is_expected.to eq false
+  		end
+  	end
+
+  	context "emailカラムのテスト" do
+  		before do
+  		  @user = FactoryBot.build(:user)
+  		end
+  		it '重複したemailが存在する場合は登録できないこと' do
+	  		@user.save
+	  		another_user = FactoryBot.build(:user)
+	  		another_user.email = @user.email
+	  		another_user.valid?
+	  		expect(another_user.errors.full_messages).to include('メールアドレスは既に使用されています。')
+  		end
   	end
   end
 end
