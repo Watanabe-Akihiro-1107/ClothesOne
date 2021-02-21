73f96c5753d9a7198d3e515f3183dc98274974c8
vagrant <answer.is.near.2@gmail.com>
2021-02-21 22:32:11 +0900

    (add_rspec)model_admin

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -1,11 +1,26 @@
 require "rails_helper"
 RSpec.describe Admin ,type: :model do
-	describe "正しく保存される" do
+	context "正しく保存される" do
 		before do
 			@admin = FactoryBot.create(:admin)
 		end
 		it "全て入力されているので正しく保存される" do
-		expect(@admin).to be_valid
+			expect(@admin).to be_valid
 		end
+ 	end
+
+ 	describe "バリデーションのテスト" do
+ 	let (:admin) {build(:admin)}
+ 	subject{ admin.valid? }
+	 	context "パスワードカラムのテスト" do
+	 		it "6文字以上であること" do
+	 			admin.password = Faker::Lorem.characters(number:5)
+	 			is_expected.to eq false
+	 		end
+	 		it "空欄でないこと" do
+	 			admin.password = ""
+	 			is_expected.to be false
+	 		end
+	 	end
  	end
 end
